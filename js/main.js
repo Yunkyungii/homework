fad0e11cc20e5f2dc75146c199c5b886f870740a
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-05-17 18:17:15 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.mainSlide').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        draggable: true,
+    });
+})
+
